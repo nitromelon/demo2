0be2ab05a7d056fef9c3d16464cfbf0d826e7145
nitromelon <github.z6b5z@simplelogin.com>
2024-03-07 01:04:42 +0700

    .

--- src/app/demo.ts
+++ src/app/demo.ts
@@ -1,29 +1,31 @@
 import { Request, Response } from "express";
 import App from "./app";
 import RequestChain from "../request/chain";
-// import { PrismaClient } from "@prisma/client";
 
 export default class Demo extends App {
     override post = RequestChain.create(
         async (_req: Request, res: Response) => {
             if (this.db !== null) {
                 console.log("Database is connected yay");
-                await this.db.user.create({
-                    data: {
-                        id: Math.floor(Math.random() * 1000),
-                        username: `"Alice"${Math.floor(Math.random() * 1000)}`,
-                        email: `test${Math.floor(
-                            Math.random() * 1000
-                        )}@google.com`,
-                        password: `"123456"${Math.floor(Math.random() * 1000)}`,
-                    },
-                });
-                console.log(await this.db.user.findMany());
+                // await this.db.user.create({
+                //     data: {
+                //         id: Math.floor(Math.random() * 1000),
+                //         username: `"Alice"${Math.floor(Math.random() * 1000)}`,
+                //         email: `test${Math.floor(
+                //             Math.random() * 1000
+                //         )}@google.com`,
+                //         password: `"123456"${Math.floor(Math.random() * 1000)}`,
+                //     },
+                // });
+                // console.log(await this.db.user.findMany());
+                throw new Error("Database is not connected");
             }
 
             res.status(201).json({ message: "POST request to /page" });
         }
-    );
+    ).add_middleware((_req: Request, _res: Response, _next) => {
+        throw new Error("Middleware error test hehe");
+    })
 
     override get = RequestChain.create((req: Request, res: Response) => {
         res.status(200).json({
--- src/request/chain.ts
+++ src/request/chain.ts
@@ -1,19 +1,30 @@
-import { RequestHandler, Request, Response } from "express";
+import { RequestHandler, Request, Response, NextFunction } from "express";
 
 const defaultHandler: RequestHandler = (req: Request, res: Response) =>
     res.status(404).json({ message: `Cannot ${req.method} ${req.url}` });
+
+const asyncErrorHandler =
+    (fn: RequestHandler): RequestHandler =>
+    (req: Request, res: Response, next: NextFunction) =>
+        Promise.resolve(fn(req, res, next)).catch(next);
+
+const errorLog: any = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+};
 
 // This file is for chain request: middlewares then handler
 class RequestChain {
     middlewares: RequestHandler[] = [];
     handler: RequestHandler;
+    errorHandler = errorLog as RequestHandler;
 
     constructor(handler: RequestHandler) {
         this.handler = handler;
     }
 
     static create(handler: RequestHandler): RequestChain {
-        return new RequestChain(handler);
+        return new RequestChain(asyncErrorHandler(handler));
     }
 
     static default(): RequestChain {
@@ -21,12 +32,12 @@ class RequestChain {
     }
 
     add_middleware(middleware: RequestHandler): RequestChain {
-        this.middlewares.push(middleware);
+        this.middlewares.push(asyncErrorHandler(middleware));
         return this;
     }
 
     export(): RequestHandler[] {
-        return [...this.middlewares, this.handler]
+        return [...this.middlewares, this.handler, this.errorHandler];
     }
 }
 
