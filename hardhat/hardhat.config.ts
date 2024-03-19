8592fbb8017cb5ee3065c393798a791b08ae9312
nitromelon <github.z6b5z@simplelogin.com>
2024-03-19 15:40:38 +0700

    .

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    networks: {
+        geth: {
+            url: "http://localhost:8545",
+            chainId: 1214,
+        },
+    },
 };
 
 export default config;
