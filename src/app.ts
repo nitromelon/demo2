c34daf5f31cbd4a07bc27ed6b08e041c3f2850d6
nitromelon <github.z6b5z@simplelogin.com>
2024-04-01 00:48:03 +0700

    SWE30003 !!!

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,7 @@
 import dotenv from "dotenv";
 import InnoBE from "./request/request";
 import InnoDB from "./request/database";
-// import Demo from "./app/demo";
-import InnoWeb3 from "./request/web3";
-import Wallet from "./app/wallet";
-import WalletSend from "./app/wallet_send";
-import User from "./app/user";
-import UploadContent from "./app/upload_content/upload_content";
-import contract from "../hardhat/artifacts/contracts/Transaction.sol/Transaction.json";
-import Gallery from "./app/gallery";
-import GalleryPage from "./app/gallery_page";
-import Product from "./app/product";
-import ProductRelated from "./app/product_related";
-import MarketRate from "./app/market_rate";
-import Search from "./app/search";
+import Demo from "./app/demo";
 
 dotenv.config();
 
@@ -43,22 +31,8 @@ if (
 
 InnoDB.getSelf();
 
-InnoWeb3.getSelf()
-    .withURL(gethURL, gethPort)
-    .withAdminPrivateKey(adminPrivateKey)
-    .withSmartContract(contractAddress, contract.abi);
-
 InnoBE.create("0.0.0.0", parseInt(backendPort), parseInt(fronendPort))
     .config()
-    .route("/wallet", new Wallet())
-    .route("/wallet/transactions", new WalletSend())
-    .route("/user", new User())
-    .route("/gallery", new Gallery())
-    .route("/gallery/:category", new GalleryPage())
-    .route("/product", new Product())
-    .route("/product/:product/related", new ProductRelated())
-    .route("/search", new Search())
-    .route("/market_rate", new MarketRate())
-    .route("/upload_content", new UploadContent()) // <- This one is to upload from json to mongodb | DEV ONLY
+    .route("/demo", new Demo())
     .catch()
     .start();
--- src/app/market_rate.ts
+++
@@ -1,8 +0,0 @@
-import RequestChain from "../request/chain";
-import App from "./app";
-
-export default class MarketRate extends App {
-    override post = RequestChain.create((_req, res) => {
-        res.status(200).send({ number: 1234.5 });
-    });
-}
